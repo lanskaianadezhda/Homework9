8bc1a0de6b11083355ff91314bee37193490364d
lanskaianadezhda <nadin-91@bk.ru>
2024-10-08 15:57:13 +0500

    Сделано ДЗ9

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         //task 1
         System.out.println("Задача 1");
 
-        int[] expenses = {15125 , 10123 , 5687 , 13444 , 8936};
+        int[] expenses = {15125, 10123, 5687, 13444, 8936};
         int total = 0;
         for (int totalAll : expenses) {
             total += totalAll;
@@ -13,7 +13,7 @@ public class Main {
         //task 2
         System.out.println("Задача 2");
 
-        int[] expenses1 = {10088 , 8026 , 3000 , 12325 , 5828};
+        int[] expenses1 = {10088, 8026, 3000, 12325, 5828};
         int min = expenses1[0];
         int max = expenses1[0];
         for (int totalall1 : expenses1) {
@@ -25,5 +25,24 @@ public class Main {
             }
         }
         System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. Максимальная сумма трат за неделю составила " + max);
+
+        //task3
+        System.out.println("Задача 3");
+
+        int[] expenses2 = {1000, 802, 350, 1235, 828};
+        int total1 = 0;
+        for (int totalAll2 : expenses2) {
+            total1 += totalAll2;
+        }
+        double average = total1 / expenses2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рубля");
+
+        //task4
+        System.out.println("Задача 4");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
